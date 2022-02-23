a526bad4c62747721d65d70128bf6f7db868127d
Michael Ivanov <ivanovmichael@ufl.edu>
2022-02-23 18:35:08 -0500

    added room status and gender input forms to sign up page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { EmailFormComponent } from './email-form/email-form.component';
 import { FooterComponent } from './footer/footer.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SidebarService } from './navbar/sidebar.service';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,8 @@ import { SidebarService } from './navbar/sidebar.service';
     MatToolbarModule,
     FormsModule,
     ReactiveFormsModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatSelectModule
   ],
   providers: [SidebarService],
   bootstrap: [AppComponent]
