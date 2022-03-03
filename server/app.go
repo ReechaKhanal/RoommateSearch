405deec90a1c067522ae1fd7a1c2933f834fb20a
Michael Ivanov <ivanovmichael@ufl.edu>
2022-03-03 15:07:20 -0500

    added password hashing for user sign up

--- server/app.go
+++ server/app.go
@@ -110,28 +110,33 @@ func (a *App) login(w http.ResponseWriter, r *http.Request) {
 
 func (a *App) sign_Up(w http.ResponseWriter, r *http.Request) {
 	// Decode sign_Up info
-	var user1 User
-	err := json.NewDecoder(r.Body).Decode(&user1)
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		http.Error(w, err.Error(), 400)
 		return
 	}
 	// Find if user is in database
-	err = a.db.First(&user1.Login_var, "email = ?", user1.Login_var.Email).Error
+	err = a.db.First(&user.Login_var, "email = ?", user.Login_var.Email).Error
 	if err != gorm.ErrRecordNotFound {
 		http.Error(w, "Invalid email", 400)
 		return
 	}
-
-	err = a.db.Create(&user1).Error // pass pointer of data to Create
+	// Hash the password before storing in db
+	user.Login_var.Password, err = hashPassword(user.Login_var.Password)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	err = a.db.Create(&user).Error
 	if err != nil {
 		http.Error(w, "Invalid entry", 400)
 		return
-	} // returns error
+	}
 
 	// Create JWT token and set cookie
 	expirationTime := time.Now().Add(5 * time.Minute)
-	tokenString, err := createToken(user1.Login_var.Username, expirationTime)
+	tokenString, err := createToken(user.Login_var.Username, expirationTime)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
