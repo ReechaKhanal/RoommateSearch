d547abd7ee3266265d4c90ff7f717079da3c4655
Reecha Khanal <reecha.khanal@gmail.com>
2022-03-21 12:23:24 -0400

    Added Application logic to the chat functionality; Closes #126

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SocketService } from "../socket.service";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  /** Chat Logic for our application */
+  messages: Array<any>;
+  chatBox: string;
 
-  ngOnInit(): void {
+  // In the constructor method we inject our provider and intialize our public variables that are bound to the UI
+  constructor(private socket: SocketService) {
+    this.messages = [];
+    this.chatBox = "";
+  }
+
+  // Since it is not a good idea to load or subscribe to events within the constructor method so instead we use the ngOnInit method.
+  ngOnInit() {
+
+    // Here, we are subscribing to the event listener we had created in the provider class.
+    // We check to see what kind of event we found.
+    // If the event is a message then we check to see if there was a sender and prepend it to the message.
+    this.socket.getEventListener().subscribe((event: { type: string; data: { content: any; sender: string; }; }) => {
+        if(event.type == "message") {
+            let data = event.data.content;
+            if(event.data.sender) {
+                data = event.data.sender + ": " + data;
+            }
+            this.messages.push(data);
+        }
+
+        // The messages starting with a slash represent sytem messages. We will later bold these system messages.
+        if(event.type == "close") {
+            this.messages.push("/The socket connection has been closed");
+        }
+        if(event.type == "open") {
+            this.messages.push("/The socket connection has been established");
+        }
+    });
+  }
+
+  //Upon destruction, the close event is sent to the server and if the chatbox is sent, the message is sent to the server.
+  ngOnDestroy(): void {
+    this.socket.close();
+  }
+
+  send() {
+    if(this.chatBox) {
+        this.socket.send(this.chatBox);
+        this.chatBox = "";
+    }
+  }
+
+  isSystemMessage(message: string) {
+    return message.startsWith("/") ? "<strong>" + message.substring(1) + "</strong>" : message;
   }
 
 }
