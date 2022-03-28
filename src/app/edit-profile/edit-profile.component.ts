881f13101a83d5692fbd68836b88cd5cca515167
Reecha Khanal <reecha.khanal@gmail.com>
2022-03-27 21:40:52 -0400

    added upload profile picture or change profile picture feature to the update preferences or the profile-edt page

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -8,6 +8,10 @@ import { Location } from '@angular/common';
 })
 export class EditProfileComponent implements OnInit {
 
+  selectedFile: File | undefined;
+  fileName = 'example.png';
+  base64Image = '';
+
   constructor(private _location: Location) { }
 
   ngOnInit(): void {
@@ -19,6 +23,46 @@ export class EditProfileComponent implements OnInit {
 
   onSaveChanges(){
     // this should be where the backend api to update user info must be called from
+  }
+
+  onFileSelected(event: Event): void {
+    console.log(event);
+    const target = event.target as HTMLInputElement;
+    const files = target.files as FileList;
+    this.selectedFile = files[0];
+    if (this.selectedFile) {
+      this.fileName = this.selectedFile.name;
+      const reader = new FileReader();
+      reader.onload = this._handleReaderLoaded.bind(this);
+      reader.readAsBinaryString(this.selectedFile);
+    }
+    //
+    // this.backendService.uploadImage(this.base64Image).subscribe(
+    //     (response) => {
+    //       console.log('response received: ', response);
+    //       this.uploadedImage = true;
+    //     },
+    //     (error) => {
+    //       console.log('Error Uploading the Image');
+    //     }
+    // );
+  }
+
+  _handleReaderLoaded(readerEvt: ProgressEvent<FileReader>): void {
+    const target = readerEvt.target;
+    if (!target) {
+      return;
+    }
+    const binaryString = target.result;
+    if (typeof binaryString === 'string') {
+      this.base64Image = btoa(binaryString);
+    }
+    console.log(this.base64Image);
+
+      // Code to convert base64 string back to image:
+      // var image = new Image();
+      // image.src = 'data:image/png;base64,' + this.base64Image;
+      // document.body.appendChild(image);
   }
 
 }
