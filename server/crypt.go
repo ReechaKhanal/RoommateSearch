d85aae92480e22b03a2aec248102c324123b8887
Michael Ivanov <ivanovmichael@ufl.edu>
2022-03-29 16:53:34 -0400

    Closes #53; finished JWT functionality and created an API to get currently logged in user's info

--- server/crypt.go
+++ server/crypt.go
@@ -3,8 +3,11 @@ package main
 import (
 	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
+	"net/http"
 	"time"
 )
+
+var jwtKey = []byte("example_key")
 
 func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -21,9 +24,13 @@ type Claims struct {
 	jwt.StandardClaims
 }
 
+type Token struct {
+	claims *Claims
+	jwt    *jwt.Token
+}
+
 func createToken(username string, expirationTime time.Time) (string, error) {
 	// Should use real key from .env
-	jwtKey := []byte("example_key")
 	claims := Claims{
 		Username: username,
 		StandardClaims: jwt.StandardClaims{
@@ -32,4 +39,45 @@ func createToken(username string, expirationTime time.Time) (string, error) {
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(jwtKey)
+}
+
+func VerifyToken(w http.ResponseWriter, r *http.Request) (*Token, error) {
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			// If the cookie is not set, return an unauthorized status
+			w.WriteHeader(http.StatusUnauthorized)
+			return nil, err
+		}
+		// For any other type of error, return a bad request status
+		w.WriteHeader(http.StatusBadRequest)
+		return nil, err
+	}
+
+	// Get the JWT string from the cookie
+	tknStr := c.Value
+
+	// Initialize a new instance of `Claims`
+	claims := &Claims{}
+
+	// Parse the JWT string and store the result in `claims`.
+	// Note that we are passing the key in this method as well. This method will return an error
+	// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+	// or if the signature does not match
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return nil, err
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return nil, err
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return nil, err
+	}
+	return &Token{claims, tkn}, nil
 }
--- server/models.go
+++ server/models.go
@@ -9,7 +9,7 @@ type Login struct {
 	Username string
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	User_id  int
+	UserID   int
 }
 
 type User struct {
@@ -20,7 +20,6 @@ type User struct {
 	Age        int
 	Occupation string
 	Image      string
-	Place_id   int
 	Place_var  Place
 	Login_var  Login
 
@@ -31,7 +30,7 @@ type User struct {
 
 type Preferences struct {
 	gorm.Model
-	User_id    int
+	UserID     int
 	Gender     string
 	Drinking   bool
 	Smoking    bool
@@ -43,7 +42,7 @@ type Preferences struct {
 type Place struct {
 	gorm.Model
 	Id          int
-	User_id     int
+	UserID      int
 	latitude    float32 `sql:"type:decimal(8,6);"`
 	longitude   float32 `sql:"type:decimal(9,6);"`
 	Price       float64
@@ -51,41 +50,41 @@ type Place struct {
 	Description string
 }
 
-func dummySet(db *gorm.DB) {
-
-	logins := []Login{
-		{Username: "Test User 1", Email: "testuser1@email.com", Password: "Testuser1", User_id: 0001},
-		{Username: "Test User 2", Email: "testuser2@email.com", Password: "Testuser2", User_id: 0002},
-	}
-	for _, l := range logins {
-		db.Create(&l)
-	}
-
-	users := []User{
-		{Id: 0001, Name: "Test User 1", Gender: "Female", Age: 22, Occupation: "Doctor", Place_id: 01},
-		{Id: 0002, Name: "Test User 2", Gender: "Male", Age: 22, Occupation: "Doctor", Place_id: 02},
-	}
-	for _, u := range users {
-		db.Create(&u)
-	}
-
-	preferences := []Preferences{
-		{User_id: 0001, Gender: "Male", Drinking: true, Smoking: false, Quite_time: "00:00 - 06:00", Pets: true, Occupation: "Architect"},
-		{User_id: 0002, Gender: "Male", Drinking: true, Smoking: false, Quite_time: "00:00 - 06:00", Pets: false, Occupation: "Lawyer"},
-	}
-	for _, p := range preferences {
-		db.Create(&p)
-	}
-
-	places := []Place{
-		{User_id: 0001, Id: 0001, Price: 1000, Bedrooms: 2, Description: "Newly Renovated Home"},
-		{User_id: 0002, Id: 0002, Price: 1500, Bedrooms: 3, Description: "Indoor pool available"},
-	}
-	for _, pl := range places {
-		db.Create(&pl)
-	}
-
-}
+//func dummySet(db *gorm.DB) {
+//
+//	logins := []Login_var{
+//		{Username: "Test User 1", Email: "testuser1@email.com", Password: "Testuser1", User_id: 0001},
+//		{Username: "Test User 2", Email: "testuser2@email.com", Password: "Testuser2", User_id: 0002},
+//	}
+//	for _, l := range logins {
+//		db.Create(&l)
+//	}
+//
+//	users := []User{
+//		{Id: 0001, Name: "Test User 1", Gender: "Female", Age: 22, Occupation: "Doctor", Place_id: 01},
+//		{Id: 0002, Name: "Test User 2", Gender: "Male", Age: 22, Occupation: "Doctor", Place_id: 02},
+//	}
+//	for _, u := range users {
+//		db.Create(&u)
+//	}
+//
+//	preferences := []Preferences{
+//		{User_id: 0001, Gender: "Male", Drinking: true, Smoking: false, Quite_time: "00:00 - 06:00", Pets: true, Occupation: "Architect"},
+//		{User_id: 0002, Gender: "Male", Drinking: true, Smoking: false, Quite_time: "00:00 - 06:00", Pets: false, Occupation: "Lawyer"},
+//	}
+//	for _, p := range preferences {
+//		db.Create(&p)
+//	}
+//
+//	places := []Place{
+//		{User_id: 0001, Id: 0001, Price: 1000, Bedrooms: 2, Description: "Newly Renovated Home"},
+//		{User_id: 0002, Id: 0002, Price: 1500, Bedrooms: 3, Description: "Indoor pool available"},
+//	}
+//	for _, pl := range places {
+//		db.Create(&pl)
+//	}
+//
+//}
 
 func setup(db *gorm.DB) {
 	db.AutoMigrate(&User{}, &Login{}, &Preferences{}, &Place{})
