2ca5141da3243ac9a29c108a0fc940e18a932fdc
Reecha Khanal <reecha.khanal@gmail.com>
2022-04-20 00:42:27 -0400

    Closes #206; Cypress Test for Home Page Updated to include the changed location functionality

--- cypress/integration/Roomate Search Tests/Startup-Login-Signup/AddressFilterFunctionality.js
+++ cypress/integration/Roomate Search Tests/Startup-Login-Signup/AddressFilterFunctionality.js
@@ -5,6 +5,8 @@ describe('Test Home Page Functionality', () => {
 	beforeEach(() => {
 
 	    // Visit http://localhost:4200/ to start testing our application
+
+      cy.viewport( 1270, 800)
 	    cy.visit('http://localhost:4200/');
 
       // Check if the login page loads correctly
@@ -22,28 +24,30 @@ describe('Test Home Page Functionality', () => {
   it("Home Page Functionality", () => {
 
     // Get the location bar and enter the location - University of Florida, Gainesville, Florida
-    cy.get('*[class^="app-home-map"]').type('University of Florida, Gainesville, Florida')
+    cy.get('*[class^="app-home-map"]').type('Tampa, Hillsborough County, Florida, USA')
 
     // wait for 500 milliseconds - give client enough time to load the locations
     cy.wait(500)
+    cy.wait(500)
 
-    cy.get('*[class^="results active"]')
-    cy.get('*[data-key^=0]').click()
+    cy.get('*[role^="listbox"]')
+    cy.get('*[role^="option"]').first().click()
 
     // Check if the Home Page contains user-cards
     cy.get('.user-card');
 
-    // Check if the user-cards contain a send a message button
-    cy.get('.user-card').contains('Send a message');
-    cy.get('.sendMessageButton').first().click();
+    cy.get('*[class^="mat-focus-indicator submitButton"]').click()
 
-    cy.wait(500)
+    // Check if the user-cards contain a send a message button
+    //cy.get('.user-card').contains('Send a message');
+    //cy.get('.sendMessageButton').first().click();
+
+    //cy.wait(500)
     // Check if clicking on the send message button from home page takes you to the chat page
-    cy.contains('Chat History');
+    //cy.contains('Chat History');
 
     // Navigate Back to Home Page
-    cy.get('.nav-home')
-    cy.get('.nav-home').click()
-
+    //cy.get('.nav-home')
+    //cy.get('.nav-home').click()
   })
 })
